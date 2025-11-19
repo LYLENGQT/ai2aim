875e0f0dc6114b276e21bf95e7e6c71050028a91
LYLENGQT <lylengqt@gmail.com>
2025-11-19 14:48:35 +0800

    Update Vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
-
+ 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
     fs: {
-      allow: ["./client", "./shared"],
+      allow: [path.resolve(__dirname, "client"),
+      path.resolve(__dirname, "shared"),
+      path.resolve(__dirname)],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
@@ -24,16 +26,17 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
+ 
 function expressPlugin(): Plugin {
   return {
     name: "express-plugin",
     apply: "serve", // Only apply during development (serve mode)
     configureServer(server) {
       const app = createServer();
-
+ 
       // Add Express app as middleware to Vite dev server
       server.middlewares.use(app);
     },
   };
 }
+ 
